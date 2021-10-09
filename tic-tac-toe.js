d331cd54156b9dd05157de1b279cbeccad99b271
Kieranator5050 <thejugg63@gmail.com>
2021-10-09 12:03:45 -0500

    Added Click Functionality to divs, inserts an X or O

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,12 +1,37 @@
-function styleBoard(divs){
+let gamestatus = 1;
+function checkStatus(status, div){
+  if(status%2==0){
+    //console.log("O");
+    div.innerHTML = "O";
+  } else {
+    //console.log("X");
+    div.innerHTML = "X";
+  }
+}
+
+function initBoard(divs){
   //Function to style all elements of the board
   //Assuming parameter is array of divs
   for(let i = 0; i<divs.length; i++){
-    //console.log(divs[i]);
+
+    //Set class to Square
     divs[i].className = "square";
+
+    //Add Event Listeners
+    let isClicked = false;
+    divs[i].addEventListener("click", ()=>{
+      //If not clicked check for the following
+      if(!isClicked){
+          //console.log("Clicked , ", divs[i]);
+          isClicked = true;
+          //Pass in game status parameter
+          checkStatus(gamestatus, divs[i]);
+          gamestatus++;
+      }
+    });
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    styleBoard(document.getElementById('board').getElementsByTagName("div"));
+    initBoard(document.getElementById('board').getElementsByTagName("div"));
 });
